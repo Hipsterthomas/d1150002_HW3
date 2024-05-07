7df591dc9c12c5519a7bfa6a3c9da18286d4ff03
ben <ben921107@gmail.com>
2024-05-07 15:26:19 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector("button");
+function  click(){
+    var email = document.getElementById("emailin").value;
+    var passwd = document.getElementById("passwdin").value;
+    if(!email || !passwd){
+        alert("Please enter your Email and Password! OR U WILL GET FLUNK");
+    }
+    else{
+        alert("Congratulations!! You have been hacked\n"+"Your Email is: "+email+"\nYour Password is: "+passwd);
+        alert("SO GO TO STUDY YOU LITTLE \"DUMB\" ASS!!!");
+        setTimeout(window.location.href = "https://ilearn.fcu.edu.tw/",500)
+    }
+}
+btn.addEventListener("click",click);
